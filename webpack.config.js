1353559684ca223a2c73e9dbf841b41eb9265600
Charpell <charpellumeh@gmail.com>
2018-04-19 15:28:37 +0100

    Reducer Playground

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/playground/redux-expensify.js',
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'bundle.js'
