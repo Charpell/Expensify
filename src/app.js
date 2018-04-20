30fa4be0ead87d8e886a6e5bf1b1c560740bdd4d
Charpell <charpellumeh@gmail.com>
2018-04-20 07:28:34 +0100

    Controlled Input for filters and Delete Item

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }))
 store.dispatch(addExpense({ description: 'Gas bill' }))
-store.dispatch(setTextFilter('r'))
+store.dispatch(setTextFilter('water'))
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
