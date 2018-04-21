5f321b819ad81265ff589653b145de0ec965de72
Charpell <charpellumeh@gmail.com>
2018-04-21 16:04:57 +0100

    Use Extract-Text-Plugin to seperate the CSS file from the bundle.js and place it in its own file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
   isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css')
 
   console.log('env', env)
   return {
@@ -20,15 +22,29 @@ module.exports = (env) => {
       }, 
       {
         test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: CSSExtract.extract({
+          use: [ 
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
       }]
     },
   
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
   
     devServer: {
       contentBase: path.join(__dirname, 'public'),
