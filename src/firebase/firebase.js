d268580d3da1c49dbcda0fb42cdd7a76797e7742
Charpell <charpellumeh@gmail.com>
2018-04-21 20:17:54 +0100

    Setup and Configure Firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyAGtv3h0A0lMpYI927xgX8AOYVOCv4yXVo",
+  authDomain: "expensify-e054c.firebaseapp.com",
+  databaseURL: "https://expensify-e054c.firebaseio.com",
+  projectId: "expensify-e054c",
+  storageBucket: "expensify-e054c.appspot.com",
+  messagingSenderId: "878785967539"
+};
+
+firebase.initializeApp(config);
+
+
+firebase.database().ref().set({
+  name: 'John Men'
+})
