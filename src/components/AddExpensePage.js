6b3e04b891d21b8b0c708137880179513f6eb1fc
Charpell <charpellumeh@gmail.com>
2018-04-21 10:41:04 +0100

    Use Map Dispatch To Props

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -10,11 +10,16 @@ const AddExchangePage = (props) => (
     <h1> Add Expense </h1>
     <ExpenseForm 
       onSubmit={(expense) => {
-        props.dispatch(addExpense(expense))
+        {/* props.dispatch(addExpense(expense)) */}
+        props.onSubmit(expense)
         props.history.push('/')
       }}
     />
   </div>
 )
 
-export default connect()(AddExchangePage);
+const mapDispatchToProps = (dispatch) => ({
+  onSumbit: (expense) => dispatch(addExpense(expense))
+})
+
+export default connect(undefined, mapDispatchToProps)(AddExchangePage);
