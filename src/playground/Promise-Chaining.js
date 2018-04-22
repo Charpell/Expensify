e6786f2a3251e74e086c7c73a1cdeffd55add51f
Charpell <charpellumeh@gmail.com>
2018-04-22 15:07:33 +0100

    Testing Async Redux Action 2

--- src/playground/Promise-Chaining.js
+++ src/playground/Promise-Chaining.js
@@ -10,19 +10,33 @@ const promise = new Promise((resolve, reject) => {
 })
 
 
-promise.then((data) => {
-  console.log('First data: \n', data)
-}).catch((error) => {
-  console.log('Error Messsage: \n', error)
-}) 
+// promise.then((data) => {
+//   console.log('First data: \n', data)
+// }).catch((error) => {
+//   console.log('Error Messsage: \n', error)
+// }) 
 
 
 // Promise Chaining
+// promise.then((data) => {
+//   console.log('First data: \n', data)
+//   return data
+// }).then((secondData) => {
+//   console.log('Second data: \n', secondData.name)
+// }).catch((error) => {
+//   console.log('Error Messsage: \n', error)
+// })
+
+// Chaining Promise
 promise.then((data) => {
   console.log('First data: \n', data)
-  return data
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('This is my second Promise')
+    }, 5000)
+  })
 }).then((secondData) => {
-  console.log('Second data: \n', secondData.name)
+  console.log('Second data: \n', secondData)
 }).catch((error) => {
   console.log('Error Messsage: \n', error)
 })
