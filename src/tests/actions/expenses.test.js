f680b85d1f031da40f365288da5b1606626b1003
Charpell <charpellumeh@gmail.com>
2018-04-22 16:37:45 +0100

    Remove env files

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -91,17 +91,3 @@ test('should add expense with defaults to database and store', (done) => {
   });
 
 })
-
-// test('should setup add expense action object with default values', () => {
-//   const action = addExpense();
-//   expect(action).toEqual({
-//     type: 'ADD_EXPENSE',
-//     expense: {
-//       id: expect.any(String),
-//       description: '',
-//       note: '',
-//       amount: 0,
-//       createdAt: 0
-//     }
-//   })
-// })
