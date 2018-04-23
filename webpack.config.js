86d3db389c00c1806a3ebfe4fd239d7b5a80fd44
Charpell <charpellumeh@gmail.com>
2018-04-23 10:23:38 +0100

    Prevents heroku from crashing

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,18 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if (process.env.NODE_ENV === 'test') {
-  require('dotenv').config({ path: '.env.test'});
-} else if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config({ path: '.env.development'});
+
+// Prevents Heroku from crashing
+try {
+  if (process.env.NODE_ENV === 'test') {
+    require('dotenv').config({ path: '.env.test'});
+  } else if (process.env.NODE_ENV === 'development') {
+    require('dotenv').config({ path: '.env.development'});
+  }
+} catch (error) {
+  /**/
 }
+
 
 module.exports = (env) => {
   isProduction = env === 'production';
